f867871a328325ff1502d5e07cca87cb51321589
YogeshBohora <borat2335@gmail.com>
2023-02-06 20:53:25 +0545

    first commit

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.profiles, name='profiles'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
+    path('user-profile/<str:pk>/', views.userProfile, name='user-profile')
+]
